100942a8937dbea7c7abb3d4fdbff9803050cd93
xadla <hadinjr4122@gmail.com>
2025-03-30 14:17:06 +0330

    add style for navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <h1>Welcome to Home Page</h1>;
+  return <h1 className="text-5xl font-bold text-center">Welcome to Home Page</h1>;
 }
 
 export default Home;
